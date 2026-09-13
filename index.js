ed24195afef8c7edb8cd1b209ccc1111954bc14c
skfantasmaaaaa-ctrl <sk.fantasmaaaaa@gmail.com>
2026-09-13 15:20:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-console.log("💎 Safira Bot iniciada!");
-console.log("✨ Sistema funcionando!");
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason
+} = require("@whiskeysockets/baileys");
+
+const Pino = require("pino");
+
+async function iniciarSafira() {
+  const { state, saveCreds } = await useMultiFileAuthState("./safira-session");
+
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: false,
+    logger: Pino({ level: "silent" })
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  if (!sock.authState.creds.registered) {
+    const numero = "55SEUNUMERO";
+
+    const codigo = await sock.requestPairingCode(numero);
+
+    console.log("💎 CÓDIGO DA SAFIRA BOT:");
+    console.log(codigo);
+  }
+
+  sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
+    if (connection === "open") {
+      console.log("💎 Safira Bot conectada ao WhatsApp!");
+    }
+
+    if (connection === "close") {
+      const deveReconectar =
+        lastDisconnect?.error?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+
+      if (deveReconectar) {
+        iniciarSafira();
+      }
+    }
+  });
+}
+
+iniciarSafira();
